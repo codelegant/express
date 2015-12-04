3fec766e0657ff983029a419cccf4fc52cf9f395
Laichuanfeng <elegant.lai@kiwifi.cn>
2015-12-04 22:08:19 +0800

    界面的初步处理与菜单的响应

--- myapp/routes/user.js
+++ myapp/routes/user.js
@@ -1,22 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 var pool = mysql.createPool({
-	host: 'localhost',
-	user: 'root',
-	password: 'root',
-	database: 'xucr'
+	host: "localhost",
+	user: "root",
+	password: "root",
+	database: "xucr"
 });
 
-router.route('/signup')
+router.route("/signup")
 	.get(function (req, res) {
-		res.render('signup', { title: '注册', path: req.path });
+		res.render("signup", { title: "注册", path: req.path});
+	})
+	.post(function (req, res) {
+		console.log(req.body);
+		var params = req.body,
+			username = params.username,
+			password = params.password,
+			passwordAgain = params.password_again;
+		if (password === passwordAgain) {
+		} else {
+			res.render("signup",{title:"注册",path:req.path,msg:"两次密码不匹配，请重新输入"});
+		}
 	});
 
-router.route('/signin')
+router.route("/signin")
 	.get(function (req, res) {
-		res.render('signin', { title: '登录', path: req.path });
+		res.render("signin", { title: "登录", path: req.path });
 	});
 
 module.exports = router;
