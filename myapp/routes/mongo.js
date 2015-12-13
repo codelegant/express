8a21c2b055902e17d5a2db4aec03b008777cd6fe
Laichuanfeng <elegant.lai@kiwifi.cn>
2015-12-13 16:41:27 +0800

    清除TS文件，ES6已经已经支持大部分ES6特性，用TS反应限制JS的语法表现。

--- myapp/routes/mongo.js
+++ myapp/routes/mongo.js
@@ -4,17 +4,9 @@ var mongoClient = mongodb.MongoClient;
 var url = "mongodb://localhost:27017/express";
 var insertDocuments = function (db, callback) {
     var collection = db.collection("user");
-    collection.insertMany([
-        { a: 1 }, { a: 2 }, { a: 3 }
-    ]).then(function (result) {
-        assert.equal(3, result.result.n);
-        assert.equal(3, result.ops.length);
-        console.log("Inserted 3 documents into the document coollection");
-        callback(result);
-    });
     collection.updateOne({ "name": "lai" }, { "name": "lai" }, { upsert: true, w: 1 }, function (err, result) {
         assert.equal(null, err);
-        assert.equal(1, result.rseult.n);
+        assert.equal(1, result.result.n);
     });
 };
 mongoClient.connect(url, function (err, db) {
